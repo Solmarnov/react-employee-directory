d30f5f51dbdb0f66c184c84bd1e1bf51038a3404
Solmarnov <solmarnov@gmail.com>
2020-07-16 21:14:54 +1000

    Updates

---
+++ precache-manifest.803a93a720d9d0fa39f712db4026396a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28368582c1068950b1c3f37d3483eaae",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "c3dabe5c5575f0422e33",
+    "url": "/react-employee-directory/static/css/main.f5094049.chunk.css"
+  },
+  {
+    "revision": "e028b4e9c319ce15a023",
+    "url": "/react-employee-directory/static/js/2.6bf72daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.6bf72daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3dabe5c5575f0422e33",
+    "url": "/react-employee-directory/static/js/main.f691933c.chunk.js"
+  },
+  {
+    "revision": "d37e1857b6464b7bd13b",
+    "url": "/react-employee-directory/static/js/runtime-main.7a81436f.js"
+  },
+  {
+    "revision": "1bbdc4e07d9ec19e69c6bd3adf509d12",
+    "url": "/react-employee-directory/static/media/adam-greenburg.1bbdc4e0.png"
+  },
+  {
+    "revision": "00246726bd8c8dbf137df50149297f68",
+    "url": "/react-employee-directory/static/media/craig-stevens.00246726.png"
+  },
+  {
+    "revision": "bbd22a49870b4b8ba7d9b80e781d51e3",
+    "url": "/react-employee-directory/static/media/dean-yates.bbd22a49.png"
+  },
+  {
+    "revision": "5e69244e3732ae1d77e87065d5c76270",
+    "url": "/react-employee-directory/static/media/gavin-phillips.5e69244e.png"
+  },
+  {
+    "revision": "7c4b601f06d476e81c2fa01cf067c68f",
+    "url": "/react-employee-directory/static/media/janet-weis.7c4b601f.png"
+  },
+  {
+    "revision": "868040fe0689f12eea93006c686a328a",
+    "url": "/react-employee-directory/static/media/karen-wicks.868040fe.png"
+  },
+  {
+    "revision": "f92180000cd50ccca9851c7a5ee342b6",
+    "url": "/react-employee-directory/static/media/magenta-adams.f9218000.png"
+  },
+  {
+    "revision": "d384eacf30e67db3dc42d9fdbfa9c258",
+    "url": "/react-employee-directory/static/media/susan-melts.d384eacf.png"
+  },
+  {
+    "revision": "22140f5a7bf00651350c468398514ffa",
+    "url": "/react-employee-directory/static/media/todd-jefferson.22140f5a.png"
+  },
+  {
+    "revision": "d73ca7c8fb65a0e48701f2b30730cf8a",
+    "url": "/react-employee-directory/static/media/tracy-waddlesworth.d73ca7c8.png"
+  }
+]);
